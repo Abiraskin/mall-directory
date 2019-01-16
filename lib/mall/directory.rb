14865d866cfbbdd8ce0c9c56e3f8f88393d9569b
Abi Raskin <abiraskin@gmail.com>
2019-01-16 03:20:06 +0000

    initial commit

---
+++ lib/mall/directory.rb
@@ -0,0 +1,8 @@
+require "mall/directory/version"
+
+module Mall
+  module Directory
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/mall/directory/version.rb
@@ -0,0 +1,5 @@
+module Mall
+  module Directory
+    VERSION = "0.1.0"
+  end
+end
